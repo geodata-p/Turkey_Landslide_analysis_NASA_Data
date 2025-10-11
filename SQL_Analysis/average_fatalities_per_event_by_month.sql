ddfda00d3da3e5e37e380c3c555a27009103b69b
geodata-p <p_shahmalekpour@yahoo.com>
2025-10-11 12:13:56 +0100

    Update average_fatalities_per_event_by_month.sql

--- SQL_Analysis/average_fatalities_per_event_by_month.sql
+++ SQL_Analysis/average_fatalities_per_event_by_month.sql
@@ -1,1 +1,11 @@
+-- Average fatalities per event by month
+SELECT
+    MONTH(event_date) AS month_num,
+    DATE_FORMAT(event_date, '%M') AS month_name,
+    COUNT(*) AS events,
+    SUM(fatality_count) AS fatalities,
+    ROUND(SUM(fatality_count) / COUNT(*), 2) AS fatalities_per_event
+FROM landslides_tr
+GROUP BY month_num, month_name
+ORDER BY month_num;
 
