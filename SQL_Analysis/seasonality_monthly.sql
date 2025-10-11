c2f8446aa622a92dbe2409122ea0715705d21fe6
geodata-p <p_shahmalekpour@yahoo.com>
2025-10-11 12:07:02 +0100

    Update seasonality_monthly.sql

--- SQL_Analysis/seasonality_monthly.sql
+++ SQL_Analysis/seasonality_monthly.sql
@@ -1,1 +1,10 @@
+-- Seasonality: Events per month (Jan–Dec)
+SELECT
+  MONTH(event_date) AS month_num,
+  DATE_FORMAT(event_date, '%M') AS month_name,
+  COUNT(*) AS events,
+  ROUND(100.0 * COUNT(*) / (SELECT COUNT(*) FROM landslides_tr), 1) AS pct_of_total
+FROM landslides_tr
+GROUP BY month_num, month_name
+ORDER BY month_num;
 
